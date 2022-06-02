804837f47db1b1c0809ff1bd491169c30f2a495d
Saurabh Nayak <saurabh.nayak@techforce.com>
2022-06-02 13:26:17 +0530

    Improvement comment messages

--- com.saurabh.DBManager/src/module-info.java
+++ com.saurabh.DBManager/src/module-info.java
@@ -1,4 +1,6 @@
 module com.saurabh.DBManager {
+    //requires used for module name
     requires com.saurabh.DBHelper;
+    //exports used to export package
     exports com.saurabh.manager;
 }
--- src/module-info.java
+++ src/module-info.java
@@ -1,3 +1,4 @@
 module ModularExample {
     requires com.saurabh.DBManager;
+    requires java.sql;//can use any module as this way
 }
